9a84d0c4daf123e673072668e7329bfc5601adc7
justAlves <iamalves.jpg@gmail.com>
2023-05-31 06:50:34 -0300

    feat: :sparkles: tratamento de errros

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
-  import express, { } from "express";
+  import express, { Request, Response, NextFunction } from "express";
+  import 'express-async-errors'
+  import cors from 'cors'
 
   const app = express()
+
+  app.use(express.json())
+  app.use(cors())
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error){
+      return res.status(400).json({
+        error: err.message
+      })
+    }
+
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error.'
+    })
+  })
 
   app.listen(3000, () => {
     console.log("Servidor rodando... 🚀")
